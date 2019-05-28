80965f66f2a472268211169f0747e33248544fc0
meholt <meholt90@gmail.com>
2019-05-27 17:43:03 -0700

    finished-array-assignment-fixing-filter-method-for-some-reason-it-returns-all-runners

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -78,19 +78,18 @@ console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-
-runners.forEach(function(currentObj, shirt_size) {
-
-
-})
+let largeShirts = runners.filter((orderedLarge) => {
+    runners.shirt_size === 'L';
+    return orderedLarge;
+});
 
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-
+let ticketPriceTotal = runners.reduce((total, runner) => {
+    return total + runner.donation;
+}, 0);
 
 console.log(ticketPriceTotal);
 
